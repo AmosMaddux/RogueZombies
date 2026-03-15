bac5b4036240db316e1a4e6be3fcffc56bb8534b
Amos Maddux <65032847+AmosMaddux@users.noreply.github.com>
2026-03-15 16:00:55 -0500

    Update player.gd
    Added animations to player

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,12 @@
 extends CharacterBody2D
 
-@onready var visuals = $Visuals
+@onready var visuals: Node2D = $Visuals
+@onready var animated_sprite: AnimatedSprite2D = $Visuals/AnimatedSprite2D
+
 const SPEED = 150.0
 const JUMP_VELOCITY = -400.0
 
+# Version Control Test
 
 func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
@@ -14,6 +17,21 @@ func _physics_process(delta: float) -> void:
 		
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, SPEED)
+		
+	#Set Animations
+	if direction.x != 0:
+		animated_sprite.animation = "walk_right" if (direction.x > 0) else "walk_left"
+		visuals.scale.x = 1 if (direction.x > 0) else -1
+	elif direction.y != 0:
+		animated_sprite.animation = "walk_up" if direction.y > 0 else "walk_down"
+		visuals.scale.x = 1
+		
+	
+	if direction != Vector2.ZERO:
+		animated_sprite.play()
+	else:
+		animated_sprite.stop()
+		animated_sprite.frame = 0
 
 	
 	move_and_slide()
