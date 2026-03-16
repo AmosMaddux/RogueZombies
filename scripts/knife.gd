bf2511c3e538d0b7fcb56e2be8f543d66742cbf6
AmosMaddux <65032847+AmosMaddux@users.noreply.github.com>
2026-03-16 17:56:54 -0500

    Last Sync: 2026-03-16 17:56 (Mobile)

--- scripts/knife.gd
+++ scripts/knife.gd
@@ -1,11 +1,25 @@
 extends Area2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var is_equipped = false
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_body_entered(body: Node2D) -> void:
+	print("Triggered!")
+	if body.is_in_group("player") and not is_equipped:
+		print("Player entered")
+		equip_to_player(body)
+		
+func equip_to_player(player: CharacterBody2D):
+	is_equipped = true
+	
+	#Disable collision so it doesn't collide while holding
+	$CollisionShape2D.set_deferred("disabled", true)
+	
+	#Add knife to weapon slot in player and remove from main scene
+	var slot = player.get_node("WeaponPivot/WeaponSlot")
+	get_parent().remove_child(self)
+	slot.add_child(self)
+	
+	# Set position and rotation local to weapon slot
+	position = Vector2.ZERO
+	rotation = 90
