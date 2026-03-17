1d8da286c3399dcbda44ab689c4038026c0ca101
AmosMaddux <65032847+AmosMaddux@users.noreply.github.com>
2026-03-17 10:17:34 -0500

    Last Sync: 2026-03-17 10:17 (Mobile)

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 
 
-
+#Vars
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+var health = 100
 
 #Child Nodes
 @onready var animated_sprite_2d: AnimatedSprite2D = $Visuals/AnimatedSprite2D
@@ -11,9 +12,18 @@ const JUMP_VELOCITY = -400.0
 func animate_damage():
 	animated_sprite_2d.play("damage_idle")
 	
+func die():
+	animated_sprite_2d.play("die")
+	await animated_sprite_2d.animation_finished
+	queue_free()
+	
 func take_damage(damage_amount: int):
-	print("Ow! " + self.name + " has taken " + str(damage_amount) + " damage!")
+	print("Ow! " + self.name + " has taken " + str(damage_amount) + " damage! Current health: " + str(health))
+	health -= damage_amount
 	animate_damage()
+	
+	if health >= 0:
+		die()
 	
 
 func _physics_process(delta: float) -> void:
