80fb4aa9ead3aa696364e1e15429bc4c743462d3
AmosMaddux <65032847+AmosMaddux@users.noreply.github.com>
2026-03-16 19:19:02 -0500

    Last Sync: 2026-03-16 19:19 (Mobile)

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -7,3 +7,8 @@ const JUMP_VELOCITY = -400.0
 
 func _physics_process(delta: float) -> void:
 	pass
+
+
+func _on_hitbox_body_entered(body: Node2D) -> void:
+	if body is Knife:
+		print("Ow..damage taken")
