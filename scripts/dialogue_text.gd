9a2534771d575397ef6ba561149f016d115b4fcd
AmosMaddux <65032847+AmosMaddux@users.noreply.github.com>
2026-04-01 15:47:22 -0500

    Last Sync: 2026-04-01 15:47 (Mobile)

--- scripts/dialogue_text.gd
+++ scripts/dialogue_text.gd
@@ -2,21 +2,49 @@ extends Label
 
 @onready var timer: Timer = $Timer
 
+var sequence_name = ""
+var sequence_num = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	change_dialogue("start")
+	GameEvents.set_dialogue.connect(change_dialogue)
+	change_dialogue("start", 0)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-func change_dialogue(event: String):
+func change_dialogue(event: String, new_sequence_num: int):
 	if event == "start":
-		text = "What... where am I?"
-		self.visible = true
-		timer.start()
+		sequence_name = event
+		
+		if new_sequence_num == 0:
+			print("0 Sequence Name: " + sequence_name + " Sequence_Num: " + str(sequence_num))
+			text = "What... where am I?"
+			self.visible = true
+			sequence_num += 1
+			timer.start()
+		elif new_sequence_num == 1:
+			print("1 Sequence Name: " + sequence_name + " Sequence_Num: " + str(sequence_num))
+			text = "Holy crap! Is that a zombie!?!?"
+			sequence_num += 1
+			timer.start()
+		elif new_sequence_num == 2:
+			print("2 Sequence Name: " + sequence_name + " Sequence_Num: " + str(sequence_num))
+			self.visible = false
+			sequence_num = 0
+			sequence_name = ""
+		else:
+			print("3 Sequence Name: " + sequence_name + " Sequence_Num: " + str(sequence_num))
+			print("Sequence num not valid")
+	else:
+		print("Sequence Name not valid")
+		
+		
+		
 
 
 func _on_timer_timeout() -> void:
-	self.visible = false
+	print("Timer ran out")
+	change_dialogue(sequence_name, sequence_num)
--- scripts/game_events.gd
+++ scripts/game_events.gd
@@ -5,6 +5,7 @@ signal player_health_changed(new_health)
 signal player_ammo_changed(new_ammo, new_reserves)
 signal weapon_changed(weapon_name)
 signal money_changed(new_money)
+signal set_dialogue(event, sequence_num)
 
 #Gate Events and Functions
 signal gate_purchased()
