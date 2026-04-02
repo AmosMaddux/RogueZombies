bc3bf90af4739c6399e3d65a55f6e2072f5580eb
AmosMaddux <65032847+AmosMaddux@users.noreply.github.com>
2026-04-02 10:47:35 -0500

    Last Sync: 2026-04-02 10:47 (Mobile)

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -6,7 +6,9 @@ extends CanvasLayer
 @onready var ammo_reserve_num: Label = $MarginContainer/Control/ColorRect/AmmoReserveNum
 @onready var weapon_icon: TextureRect = $MarginContainer/Control/ColorRect/WeaponIcon
 
-@export var knife_icon: Image
+@export var knife_icon: Texture2D
+@export var pistol_icon: Texture2D
+@export var shotgun_icon: Texture2D
 
 
 # Called when the node enters the scene tree for the first time.
@@ -24,7 +26,15 @@ func on_ammo_updated(new_ammo, new_reserves):
 	ammo_reserve_num.text = str(new_reserves)
 	
 func on_weapon_name_changed(new_weapon_name):
-	weapon_name_text.text = new_weapon_name
+	#weapon_name_text.text = new_weapon_name
+	if new_weapon_name == "knife":
+		weapon_icon.texture = knife_icon
+	elif new_weapon_name == "Pistol":
+		weapon_icon.texture = pistol_icon
+	elif new_weapon_name == "Shotgun":
+		weapon_icon.texture = shotgun_icon
+	else:
+		print("Invalid weapon name. Icon cannot be set!")
 	
 func on_money_changed(new_money):
 	money_num.text = ("$" + str(new_money))
